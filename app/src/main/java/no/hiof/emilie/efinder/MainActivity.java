f27c51c866464405c792e5377684dfa89ffbd698
emiliero <emiliero@hiof.no>
2018-10-09 12:31:08 +0200

    små endringer

--- app/src/main/java/no/hiof/emilie/efinder/MainActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/MainActivity.java
@@ -1,7 +1,9 @@
 package no.hiof.emilie.efinder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ActionBar actionBar = getActionBar();
+
+        //ActionBar vises
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        //Kalles på når brukeren bytter mellom tabs
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            public void onTabSelected (ActionBar.Tab tab, FragmentTransaction ft) {
+                //vis gitt tab
+            }
+            public void onTabUnselected (ActionBar.Tab tab, FragmentTransaction ft) {
+                //skjul gitt tab
+            }
+            public void onTabReselected (ActionBar.Tab tab, FragmentTransaction ft) {
+                //Mest sannsynlig ignorere dette eventet
+            }
+        };
+
+        //Legge til 2 tabs, spesifiser tabbenes tekst og TabListener
+        for (int i = 0; i < 2; i++) {
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText("Tab" + (i+1)) //TODO: Gi bedre navn siden
+                            .setTabListener(tabListener));
+        }
     }
 }
