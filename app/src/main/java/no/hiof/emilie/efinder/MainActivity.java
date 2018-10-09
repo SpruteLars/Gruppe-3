8ae4f7106dce8bbf869cbf6a7acf5d7efd96f1f9
Marcus <marcus.s.olsen@hiof.no>
2018-10-09 15:44:15 +0200

    Did it now
    fixed it

--- app/src/main/java/no/hiof/emilie/efinder/MainActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< Updated upstream
         textProfil = (TextView) findViewById(R.id.textProfil);
         textInstilliger = (TextView) findViewById(R.id.textInstillinger);
 
@@ -43,9 +42,6 @@ public class MainActivity extends AppCompatActivity {
                 });
 
         /*final ActionBar actionBar = getSupportActionBar();
-=======
-        /*final ActionBar actionBar = getActionBar();
->>>>>>> Stashed changes
 
         //ActionBar vises
         //actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -61,20 +57,15 @@ public class MainActivity extends AppCompatActivity {
             }
             public void onTabReselected (ActionBar.Tab tab, FragmentTransaction ft) {
                 //Mest sannsynlig ignorere dette eventet
-            }*/
-      /*  };
+            }
+        };
 
         //Legge til 2 tabs, spesifiser tabbenes tekst og TabListener
         for (int i = 0; i < 2; i++) {
             actionBar.addTab(
                     actionBar.newTab()
-                            .setText("Tab" + (i+1)) TODO: Gi bedre navn siden
+                            .setText("Tab" + (i+1)) //TODO: Gi bedre navn siden
                             .setTabListener(tabListener));
-<<<<<<< Updated upstream
         }*/
     }
-=======
-        }
-    */}
->>>>>>> Stashed changes
 }
