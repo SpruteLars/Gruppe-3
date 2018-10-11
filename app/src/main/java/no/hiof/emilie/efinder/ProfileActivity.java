fa31a7b9c6cda16a2256b9b544eadc2553b280e3
emiliero <emiliero@hiof.no>
2018-10-11 14:25:34 +0200

    Added BotNav to Profile & Settings

--- app/src/main/java/no/hiof/emilie/efinder/ProfileActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/ProfileActivity.java
@@ -1,7 +1,11 @@
 package no.hiof.emilie.efinder;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //Bottom Nav
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_profil:
+                                startActivity(new Intent(ProfileActivity.this, ProfileActivity.class));
+                                return true;
+                            case R.id.action_instillinger:
+                                startActivity(new Intent(ProfileActivity.this, SettingsActivity.class));
+                                return true;
+                        }
+                        return false;
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/no/hiof/emilie/efinder/SettingsActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/SettingsActivity.java
@@ -1,7 +1,11 @@
 package no.hiof.emilie.efinder;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        //Bottom Nav
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_profil:
+                                startActivity(new Intent(SettingsActivity.this, ProfileActivity.class));
+                                return true;
+                            case R.id.action_instillinger:
+                                startActivity(new Intent(SettingsActivity.this, SettingsActivity.class));
+                                return true;
+                        }
+                        return false;
+                    }
+                }
+        );
     }
 }
