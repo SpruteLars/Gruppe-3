91e797f5950ec4113f25868ffe27bf5c76f29828
emiliero <emiliero@hiof.no>
2018-10-11 14:36:35 +0200

    Bottom Nav added to all Activities

--- app/src/main/java/no/hiof/emilie/efinder/EventActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/EventActivity.java
@@ -1,7 +1,11 @@
 package no.hiof.emilie.efinder;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class EventActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        //Bottom Nav
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_profil:
+                                startActivity(new Intent(EventActivity.this, ProfileActivity.class));
+                                return true;
+                            case R.id.action_instillinger:
+                                startActivity(new Intent(EventActivity.this, SettingsActivity.class));
+                                return true;
+                        }
+                        return false;
+                    }
+                }
+        );
     }
 }
