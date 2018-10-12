15a3dbefa90c2b7ab180618418c3c3b5255b228b
emiliero <emiliero@hiof.no>
2018-10-12 17:20:05 +0200

    BottomNav edits

--- app/src/main/java/no/hiof/emilie/efinder/MainActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/MainActivity.java
@@ -3,25 +3,28 @@ package no.hiof.emilie.efinder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.bottomappbar.BottomAppBar;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textProfil;
-    private TextView textInstilliger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationMenuView bottomNavigationMenuView = (BottomNavigationMenuView) bottomNavigationView.getChildAt(0);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
@@ -36,5 +39,20 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        /* Vurdere om vi skal endre Bot Nav til Bot App Bar
+         * BottomAppBar bottomAppBar = (BottomAppBar) findViewById(R.id.bottom_navigation);
+         */
+
+        //Størrelse på ikoner i Bot Nav
+        for (int i = 0; i < bottomNavigationMenuView.getChildCount(); i++) {
+            final View iconView = bottomNavigationMenuView.getChildAt(i).findViewById(android.support.design.R.id.icon);
+            final ViewGroup.LayoutParams layoutParams = iconView.getLayoutParams();
+            final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+
+            //Høyde & bredde
+            layoutParams.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, displayMetrics);
+            layoutParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, displayMetrics);
+        }
     }
 }
