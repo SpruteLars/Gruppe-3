6f7ec7aedfb477824e4e49ca1a3a48d220ab9dc0
Marcus <marcus.s.olsen@hiof.no>
2018-10-16 10:43:09 +0200

    Firebase fungerer
    alle har tilgang til samme ting hvis de kan logge inn

---
+++ app/src/main/java/no/hiof/emilie/efinder/Main2Activity.java
@@ -0,0 +1,74 @@
+package no.hiof.emilie.efinder;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
+    public class Main2Activity extends AppCompatActivity {
+        private FirebaseAuth auth;
+        public Button but;
+        public EditText editEmail;
+        public EditText editPass;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
+
+            auth = FirebaseAuth.getInstance();
+            but = findViewById(R.id.myButton);
+            but.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    editEmail = findViewById(R.id.editText);
+                    editPass = findViewById(R.id.editText2);
+
+                    signIn(editEmail.getText().toString(), editPass.getText().toString());
+                }
+            });
+
+
+
+
+        }
+        private void signIn (String email, String password){
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+
+                                Toast.makeText(Main2Activity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            if (!task.isSuccessful()) {
+
+                            }
+                        }
+                    });
+
+        }
+    }
+
+
+
