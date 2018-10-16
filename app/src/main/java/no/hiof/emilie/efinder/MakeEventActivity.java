46e6cdd166258d85711844f693e9beb7fbe04ea2
emiliero <emiliero@hiof.no>
2018-10-16 10:34:22 +0200

    MakeEventActivity

---
+++ app/src/main/java/no/hiof/emilie/efinder/MakeEventActivity.java
@@ -0,0 +1,13 @@
+package no.hiof.emilie.efinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_event);
+    }
+}
