902b15dc0e36ebd6c5e6a04fb6d053b838334a18
emiliero <emiliero@hiof.no>
2018-10-16 13:54:55 +0200

    Initialise make event activity components

--- app/src/main/java/no/hiof/emilie/efinder/MakeEventActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/MakeEventActivity.java
@@ -1,13 +1,33 @@
 package no.hiof.emilie.efinder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+//import com.google.firebase.database.DatabaseReference;
 
 public class MakeEventActivity extends AppCompatActivity {
+    ImageView imageView; //Burde være array????
+    EditText textViewEventName; //Er egentlig PlainText
+    EditText textViewDate;
+    EditText textViewClock;
+    EditText textViewPayment;
+    EditText textViewAttendants;
+    EditText textViewAdresse;
+    //private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_event);
+
+        imageView = (ImageView) findViewById(R.id.imgView);
+        textViewEventName = (EditText) findViewById(R.id.txtEventName);
+        textViewDate = (EditText) findViewById(R.id.txtDate);
+        textViewClock = (EditText) findViewById(R.id.txtClock);
+        textViewPayment = (EditText) findViewById(R.id.txtPayment);
+        textViewAttendants = (EditText) findViewById(R.id.txtAttendants);
+        textViewAdresse = (EditText) findViewById(R.id.txtAdress);
+        //databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 }
