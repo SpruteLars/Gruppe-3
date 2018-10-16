505cc6465babf784ebd05e41813d9607c852d1df
Marcus <marcus.s.olsen@hiof.no>
2018-10-16 14:29:35 +0200

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/no/hiof/emilie/efinder/ProfileActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/ProfileActivity.java
@@ -6,13 +6,48 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+    String editNavn;
+    EditText editAlder;
+    EditText editTlf;
+    DatabaseReference dbref;
+    FirebaseUser user;
 
+
+    String Uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        FirebaseAuth fbdb = FirebaseAuth.getInstance();
+        dbref = FirebaseDatabase.getInstance().getReference();
+        user = fbdb.getCurrentUser();
+        Uid = user.getUid();
+
+        dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                editNavn = dataSnapshot.child(Uid).child("Navn").getValue(String.class);
+                TextView kaare = findViewById(R.id.txtProfilNavn);
+                kaare.setText(editNavn);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         //Bottom Nav
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
--- app/src/main/java/no/hiof/emilie/efinder/logInActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/logInActivity.java
@@ -1,10 +1,11 @@
 package no.hiof.emilie.efinder;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,15 +14,27 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
+import java.util.List;
 
-    public class logInActivity extends AppCompatActivity {
+
+public class logInActivity extends AppCompatActivity {
         private FirebaseAuth auth;
         public Button but;
+        ArrayAdapter<String> nisse;
         public EditText editEmail;
         public EditText editPass;
-
+        public FirebaseUser user;
+        List<String> itemlist;
+        String uid;
+        private DatabaseReference dbref;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -49,6 +62,7 @@ import com.google.firebase.auth.FirebaseAuth;
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
+                                
                                 Intent intent = new Intent(logInActivity.this, MainActivity.class);
                                 startActivity(intent);
                             } else {
