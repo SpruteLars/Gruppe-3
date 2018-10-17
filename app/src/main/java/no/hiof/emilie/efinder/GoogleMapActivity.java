14ca2646178bce8f58798ef127d27a6412bfa25c
emiliero <emiliero@hiof.no>
2018-10-17 14:07:10 +0200

    THE APP WORKS!

--- app/src/main/java/no/hiof/emilie/efinder/GoogleMapActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/GoogleMapActivity.java
@@ -1,13 +1,6 @@
 package no.hiof.emilie.efinder;
 
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-
-public class GoogleMapActivity extends FragmentActivity implements OnMapReadyCallback {
+/*public class GoogleMapActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
 
     @Override
@@ -25,4 +18,4 @@ public class GoogleMapActivity extends FragmentActivity implements OnMapReadyCal
         //Legge til lokasjonen
         //Hente fra Firebase????
     }
-}
+}*/
