0ce405b251792cbca0600c416c8cee8c849f3d3c
emiliero <emiliero@hiof.no>
2018-10-17 11:31:14 +0200

    Retrieve image name to TextView

--- app/src/main/java/no/hiof/emilie/efinder/MakeEventActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/MakeEventActivity.java
@@ -1,14 +1,15 @@
 package no.hiof.emilie.efinder;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MakeEventActivity extends AppCompatActivity {
-    ImageView imageView;
+    Button addPhotoButton;
     EditText textViewEventName;
     EditText textViewDate;
     EditText textViewClock;
@@ -16,9 +17,12 @@ public class MakeEventActivity extends AppCompatActivity {
     EditText textViewAttendants;
     EditText textViewAdresse;
     EditText textViewDescription;
+    TextView textAddedPhoto;
     Button buttonSubmit;
     //private EditText[] editTextArray;
     //private DatabaseReference databaseReference;
+    final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,7 @@ public class MakeEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_make_event);
 
         buttonSubmit = (Button) findViewById(R.id.btnSubmit);
-        imageView = (ImageView) findViewById(R.id.imgView);
+        addPhotoButton = (Button) findViewById(R.id.btnAddPhoto);
         /*textViewEventName = (EditText) findViewById(R.id.txtEventName);
         textViewDate = (EditText) findViewById(R.id.txtDate);
         textViewClock = (EditText) findViewById(R.id.txtClock);
@@ -46,7 +50,19 @@ public class MakeEventActivity extends AppCompatActivity {
                 textViewDescription = (EditText) findViewById(R.id.txtDescription),
         };
 
+        //addPhotoButton.setOnClickListener(addPhotoListener);
 
+        /** Få tak i filnavn til bildet */
+        /*Uri returnUri = returnIntent.getData();
+        Cursor returnCursor = getContentResolver().query(returnUri, null, null, null,null);
+
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        returnCursor.moveToFirst();
+        textAddedPhoto = (TextView) findViewById(R.id.txtAddPhoto);
+        textAddedPhoto.setText(returnCursor.getString(nameIndex));*/
+
+
+        /** Laste opp data til firebase */
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,4 +84,29 @@ public class MakeEventActivity extends AppCompatActivity {
             }
         });
     }
+
+    /** Håndtering av å hente bilde og ta bilde */
+    /*private View.OnClickListener addPhotoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            hentBilde();
+        }
+    };
+
+    private void hentBilde() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCOde, @Nullable Intent data) {
+        ImageView imageView = findViewById(R.id.txtAddPhoto);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCOde == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }*/
 }
