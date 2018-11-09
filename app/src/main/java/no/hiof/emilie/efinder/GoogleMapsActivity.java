01d5088ddefc5144537a85d59ca331edaf2cbc25
meglomsr <meglomsr@gmail.com>
2018-11-09 17:04:19 +0100

    Finner brukeren sin lokasjon ved å trykke på en knapp på map siden

--- app/src/main/java/no/hiof/emilie/efinder/GoogleMapsActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/GoogleMapsActivity.java
@@ -1,46 +1,184 @@
 package no.hiof.emilie.efinder;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private LatLng HIOF = new LatLng(59.12797849, 11.35272861);
+    private LatLng FREDRIKSTAD = new LatLng(59.21047628, 10.93994737);
+    public LatLng myPosition;
+    private String[] locationPermission = {Manifest.permission.ACCESS_FINE_LOCATION};
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMyLocationButtonClickListener(onMyLocationButtonClickListener);
+        mMap.setOnMyLocationClickListener(onMyLocationClickListener);
+        enableMyLocationIfPermitted();
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMinZoomPreference(5);
+        }
+        /*mMap.addMarker(new MarkerOptions().position(HIOF).title("Østfold University College"));
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(HIOF, 15, 0, 0)));*/
+
+    private void enableMyLocationIfPermitted() {
+        if (ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_PERMISSION_REQUEST_CODE);
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
     }
+
+    private void showDefaultLocation() {
+        Toast.makeText(this, "Location permission not granted, " +
+                "showing default location",
+            Toast.LENGTH_SHORT).show();
+        LatLng redmond = new LatLng(47.6739881, -122.121512);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(redmond));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocationIfPermitted();
+                } else {
+                    showDefaultLocation();
+                }
+                return;
+            }
+
+        }
+    }
+
+    private GoogleMap.OnMyLocationButtonClickListener onMyLocationButtonClickListener =
+        new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                mMap.setMinZoomPreference(4);
+                return false;
+            }
+        };
+
+
+    private GoogleMap.OnMyLocationClickListener onMyLocationClickListener =
+        new GoogleMap.OnMyLocationClickListener() {
+            @Override
+            public void onMyLocationClick(@NonNull Location location) {
+
+                mMap.setMinZoomPreference(4);
+
+                CircleOptions circleOptions = new CircleOptions();
+                circleOptions.center(new LatLng(location.getLatitude(),
+                    location.getLongitude()));
+
+                circleOptions.radius(200);
+                circleOptions.fillColor(Color.RED);
+                circleOptions.strokeWidth(6);
+
+                mMap.addCircle(circleOptions);
+            }
+        };
+
+
+    /*private class DrawRoute extends AsyncTask<LatLng, Void, PolylineOptions> {
+        @Override
+        protected PolylineOptions doInBackground(LatLng... location) {
+            LatLng startLocation = HIOF;
+            if (location[0] != null)
+                startLocation = location[0];
+
+            GMapV2Direction mapDirection = new GMapV2Direction();
+
+            Document doc = mapDirection.getDocument(startLocation, FREDRIKSTAD, GMapV2Direction.MODE_DRIVING, "AIzaSyCfEwbPIXOe6d-rYr-lz1dQTRo9Dj5vcb0");
+
+            ArrayList<LatLng> directionPoint = mapDirection.getDirection(doc);
+            PolylineOptions rectLine = new PolylineOptions().width(3).color(Color.BLUE);
+
+            for (int i = 0; i < directionPoint.size(); i++) {
+                rectLine.add(directionPoint.get(i));
+            }
+
+            return rectLine ;
+        }
+
+        @Override
+        protected void onPostExecute(PolylineOptions result) {
+            mMap.addPolyline(result);
+        }
+    }*/
+
+    /*@AfterPermissionGranted(LOCATION_PERMISSION_ID)
+    private void drawRouteFromCurrentLocation() {
+        if (EasyPermissions.hasPermissions(this, locationPermission)) {
+            LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            LatLng myPosition = new LatLng(location.getLatitude(), location.getLongitude());
+            new DrawRoute().execute(myPosition);
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, getString(R.string.no_location_permission),
+                LOCATION_PERMISSION_ID, locationPermission);
+        }
+    }*/
 }
