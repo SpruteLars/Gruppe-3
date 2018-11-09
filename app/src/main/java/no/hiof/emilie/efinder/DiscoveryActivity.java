b33fb675b431e90a1eb772693b54c679390cc029
Marcus <marcus.s.olsen@hiof.no>
2018-11-09 13:46:58 +0100

    Gitkraken er greit igjen
    På grunn av at jeg er et GENI!!

--- app/src/main/java/no/hiof/emilie/efinder/DiscoveryActivity.java
+++ app/src/main/java/no/hiof/emilie/efinder/DiscoveryActivity.java
@@ -1,21 +1,57 @@
 package no.hiof.emilie.efinder;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DiscoveryActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    FirebaseUser user;
+    DatabaseReference dbref;
+    String sookNavn;
+    Button btnSook;
+    EditText editSook;
+    HashMap<String, String> personMap;
 
-    // region onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discovery);
+        setContentView(R.layout.activity_other_users);
+        personMap = new HashMap<String, String>();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        btnSook = findViewById(R.id.btnSook);
+        editSook = findViewById(R.id.txtSokNavn);
+        btnSook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sookNavn = editSook.getText().toString();
+               // Log.d("Bruk",sookNavn);
+                DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+                DatabaseReference followingRef = rootRef.child("users");
+                ValueEventListener valueEventListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                                String followCase = ds.child("Navn").getValue(String.class);
+                                String Marcus = "Marcus Olsen";
+                                String Uid = ds.getKey();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -35,8 +71,40 @@ public class DiscoveryActivity extends AppCompatActivity {
                         return true;
                 }
                 return false;
+                                if(followCase.contains(sookNavn)){
+                                   // Log.d("Bruk", followCase);
+                                   // Log.d("Bruk", Uid);
+                                    if(personMap.containsKey(followCase) && personMap.containsValue(Uid)){
+
+                                    }else{
+                                        personMap.put(Uid, followCase);
+                                    }
+
+                                    for(String key : personMap.keySet()){
+                                        Log.d("mapper",key);
+                                    }
+                                    for(String loop : personMap.values()){
+                                        Log.d("mapper", loop);
+                                    }
+                                }
+
+                        }
+                        Intent intent = new Intent(DiscoveryActivity.this, SearchActivity.class);
+                        intent.putExtra("map", personMap);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                };
+                followingRef.addListenerForSingleValueEvent(valueEventListener);
+
             }
         });
+
+
     }
-    // endregion
+
+
 }
